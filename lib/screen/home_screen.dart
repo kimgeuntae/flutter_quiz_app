612ff9b25ed7bbd8dfc66a422b8b2e7c5991b965
kimgeuntae <kok617@naver.com>
2023-08-25 13:27:03 +0900

    Add: 퀴즈 풀기 앱 설명 추가 문제 푸는 규칙 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -20,7 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: Text('My Quiz APP'),
           backgroundColor: Colors.deepPurple,
-          leading: Container(),
+          leading: Container(), // appbar 뒤로가기 버튼 지우기
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -31,9 +31,49 @@ class _HomeScreenState extends State<HomeScreen> {
                 'images/quiz.jpeg',
                 width: width * 0.8,
               ),
-            )
+            ),
+            Padding(padding: EdgeInsets.all(width * 0.024)),
+            Text(
+              '플러터 퀴즈 앱',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '퀴즈를 풀기 전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+              textAlign: TextAlign.center,
+            ),
+            Padding(padding: EdgeInsets.all(width * 0.048)),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            _buildStep(width, '2. 문제를 잘 읽고 정답을 고른 뒤\n다음 문제 버튼을 눌러주세요.'),
+            _buildStep(width, '3. 만점을 향해 도전해보세요!'),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048,
+        width * 0.024,
+        width * 0.048,
+        width * 0.024,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(title),
+        ],
       ),
     );
   }
