74cf757dbf3e82023e0d5ea88c002e966e731a9c
kimgeuntae <kok617@naver.com>
2023-08-28 14:32:20 +0900

    Add: 뒤로가기 막기

--- lib/screen/result_screen.dart
+++ lib/screen/result_screen.dart
@@ -26,104 +26,107 @@ class ResultScreen extends StatelessWidget {
         score += 1;
       }
     }
-    return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        title: Text('My Quiz APP'),
-        backgroundColor: Colors.deepPurple,
-        leading: Container(),
-      ),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(color: Colors.deepPurple),
-            color: Colors.deepPurple,
-          ),
-          width: width * 0.85,
-          height: height * 0.5,
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: width * 0.048),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(color: Colors.deepPurple),
-                  color: Colors.white,
+    return WillPopScope(
+      onWillPop: () async => false, // 뒤로가기 못하게 처리
+      child: SafeArea(
+          child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Quiz APP'),
+          backgroundColor: Colors.deepPurple,
+          leading: Container(),
+        ),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.deepPurple),
+              color: Colors.deepPurple,
+            ),
+            width: width * 0.85,
+            height: height * 0.5,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
                 ),
-                width: width * 0.73,
-                height: height * 0.25,
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.fromLTRB(
-                          0, width * 0.048, 0, width * 0.012),
-                      child: Text(
-                        '수고하셨습니다!',
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.deepPurple),
+                    color: Colors.white,
+                  ),
+                  width: width * 0.73,
+                  height: height * 0.25,
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(
+                            0, width * 0.048, 0, width * 0.012),
+                        child: Text(
+                          '수고하셨습니다!',
+                          style: TextStyle(
+                            fontSize: width * 0.055,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '당신의 점수는',
                         style: TextStyle(
-                          fontSize: width * 0.055,
+                          fontSize: width * 0.048,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ),
-                    Text(
-                      '당신의 점수는',
-                      style: TextStyle(
-                        fontSize: width * 0.048,
-                        fontWeight: FontWeight.bold,
+                      Expanded(
+                        child: Container(),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(),
-                    ),
-                    Text(
-                      '${score.toString()}/${quizs.length.toString()}',
-                      style: TextStyle(
-                        fontSize: width * 0.15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(width * 0.012),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Container(),
-              ),
-              Container(
-                padding: EdgeInsets.only(bottom: width * 0.048),
-                child: ButtonTheme(
-                  minWidth: width * 0.73,
-                  height: height * 0.05,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomeScreen(),
+                      Text(
+                        '${score.toString()}/${quizs.length.toString()}',
+                        style: TextStyle(
+                          fontSize: width * 0.15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
                         ),
-                      );
-                    },
-                    child: Text('홈으로 돌아가기'),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(width * 0.012),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                  child: ButtonTheme(
+                    minWidth: width * 0.73,
+                    height: height * 0.05,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomeScreen(),
+                          ),
+                        );
+                      },
+                      child: Text('홈으로 돌아가기'),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    ));
+      )),
+    );
   }
 }
